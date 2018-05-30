9427eb5cefde06708f0b541557bacb007e4f2187
978740431 <978740431@qq.com>
2018-05-31 00:44:57 +0800

    ceshi

---
+++ gdemo/src/main/java/com/gdemo/GdemoApplication.java
@@ -0,0 +1,12 @@
+package com.gdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdemoApplication.class, args);
+	}
+}
